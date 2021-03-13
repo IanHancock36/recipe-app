c6cdd3ff3dd785797b8f8c2a5409a180a54c4ca2
Ian Hancock <northroadian@gmail.com>
2021-03-13 06:56:49 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React,{useEffect,useState} from 'react'
+import './App.css';
+import Recipe from './Recipe'
+const App =() => {
+  const APP_ID = '1ca76a63'
+  const APP_KEY = 'acaf524dc4fc5b3ed041818ca9ad986d'
+  
+  const [recipes, setRecipes] =useState([])
+
+
+  useEffect(() =>{
+    getRecipes();
+    
+  },[])
+  const getRecipes = async () =>{
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json()
+    setRecipes(data.hits)
+    console.log(data.hits)
+  }
+   return (
+    <div className="App">
+     
+     <form className="search-form">
+       <input className='search-bar' type="text"/>
+       <button className='searchbutton' type="submit">Search
+       </button>
+     </form>
+     {recipes.map(recipe => (
+       <Recipe key={recipe.recipe.label}title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image}/>
+     ))}
+    </div>
+  );
+}
+
+export default App;
